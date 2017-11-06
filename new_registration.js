7dcd23b713b60b69530afd1330450a7445ce42f2
AlexDem1126 <alexdem1126@gmail.com>
2017-11-06 16:21:27 -0600

    new_registration

---
+++ new_registration.js
@@ -0,0 +1,13 @@
+﻿//==============Re-Enter Password=======================
+function validateRP() {
+    var password1 = document.getElementById("password").value;
+    var password2 = document.getElementById("re_enter_password").value;
+    //alert("password1= " + password1 + ", password2= " + password2);
+
+    if (password1 === password2) {
+        document.getElementById("label_re_enter_password").innerHTML = "";
+        return;
+    } else {
+        document.getElementById("label_re_enter_password").innerHTML = "ERROR: Passwords Do Not Match. Please Re-Enter.";
+    }
+}
